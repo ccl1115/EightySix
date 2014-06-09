d5a8ba61b4735d3af99d55b581297ace9925d5b7
Simon Yu <ccl1115@gmail.com>
2014-06-09 14:08:04 +0800

    works on circle list

---
+++ EightySix/src/main/java/com/utree/eightysix/app/circle/CircleListActivity.java
@@ -0,0 +1,18 @@
+package com.utree.eightysix.app.circle;
+
+import android.os.Bundle;
+import com.utree.eightysix.R;
+import com.utree.eightysix.app.BaseActivity;
+import com.utree.eightysix.app.Layout;
+import com.utree.eightysix.app.TopTitle;
+
+/**
+ */
+@Layout (R.layout.activity_circle_list)
+@TopTitle (R.string.my_circles)
+public class CircleListActivity extends BaseActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
