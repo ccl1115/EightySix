951f8390496fdbd674d385f5bbaca3b46a25f8db
Simon Yu <ccl1115@gmail.com>
2014-06-12 10:59:15 +0800

    fixed circle search activity crash

--- EightySix/src/main/java/com/utree/eightysix/app/circle/CircleSearchActivity.java
+++ EightySix/src/main/java/com/utree/eightysix/app/circle/CircleSearchActivity.java
@@ -25,110 +25,114 @@ import java.util.Map;
 @Layout (R.layout.activity_circle_search)
 public class CircleSearchActivity extends BaseActivity {
 
-    @InjectView (R.id.lv_history)
-    public ListView mLvHistory;
+  @InjectView (R.id.lv_history)
+  public ListView mLvHistory;
 
-    @InjectView (R.id.lv_result)
-    public ListView mLvResult;
+  @InjectView (R.id.lv_result)
+  public ListView mLvResult;
 
+  private List<String> mSearchHistory;
+
+  private View mFooterClearSearch;
+
+  private final HistoryFooterViewHolder mHistoryFooterViewHolder = new HistoryFooterViewHolder();
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    getTopBar().enterSearch();
+
+    mSearchHistory = Account.inst().getSearchHistory();
+
+    mFooterClearSearch = View.inflate(this, R.layout.footer_clear_history, null);
+
+    mLvHistory.addFooterView(mFooterClearSearch, null, false);
+    U.viewBinding(mFooterClearSearch, mHistoryFooterViewHolder);
+
+    updateData();
+
+    showHistory();
+  }
+
+  @Override
+  protected void onSearchActionGo(String keyword) {
+    requestSearch(1, keyword);
+
+    for (String k : mSearchHistory) {
+      if (k.equals(keyword)) {
+        return;
+      }
+    }
+
+    mSearchHistory.add(keyword);
+
+    Account.inst().setSearchHistory(mSearchHistory);
+
+    updateData();
+  }
+
+  private void updateData() {
+    List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+
+    final String[] from = new String[]{"search"};
+    final int[] to = new int[]{R.id.tv_name};
+
+    for (String search : mSearchHistory) {
+      Map<String, String> item = new HashMap<String, String>();
+      item.put(from[0], search);
+
+      data.add(item);
+    }
+
+    mLvHistory.setAdapter(new SimpleAdapter(this, data, R.layout.item_circle_simple, from, to));
+
+    if (mSearchHistory.size() > 0) {
+      mFooterClearSearch.setVisibility(View.VISIBLE);
+    } else {
+      mFooterClearSearch.setVisibility(View.GONE);
+    }
+  }
+
+  private void clearHistory() {
+    mSearchHistory.clear();
+    Account.inst().setSearchHistory(mSearchHistory);
+
+    updateData();
+  }
+
+  private void showHistory() {
+    mLvResult.setVisibility(View.GONE);
+    mLvHistory.setVisibility(View.VISIBLE);
+  }
+
+  private void showSearchResult() {
+    mLvResult.setVisibility(View.VISIBLE);
+    mLvHistory.setVisibility(View.GONE);
+  }
+
+  private void requestSearch(int page, String keyword) {
+    request(new SearchCircleRequest(page, keyword), new OnResponse<CirclesResponse>() {
+
+      @Override
+      public void onResponse(CirclesResponse response) {
+        if (response != null) {
+
+        }
+        hideProgressBar();
+      }
+    }, CirclesResponse.class);
+    showSearchResult();
+    showProgressBar();
+  }
+
+  public class HistoryFooterViewHolder {
     @InjectView (R.id.rb_clear_history)
     public RoundedButton mRbClearHistory;
 
-    private List<String> mSearchHistory;
-
-    private View mFooterClearSearch;
-
     @OnClick (R.id.rb_clear_history)
     public void onRbClearHistory() {
-        clearHistory();
+      clearHistory();
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        getTopBar().enterSearch();
-
-        mSearchHistory = Account.inst().getSearchHistory();
-
-        mFooterClearSearch = View.inflate(this, R.layout.footer_clear_history, null);
-
-        mLvHistory.addFooterView(mFooterClearSearch, null, false);
-        U.viewBinding(mFooterClearSearch, this);
-
-        updateData();
-
-        showHistory();
-    }
-
-    @Override
-    protected void onSearchActionGo(String keyword) {
-        requestSearch(1, keyword);
-
-        for (String k : mSearchHistory) {
-            if (k.equals(keyword)) {
-                return;
-            }
-        }
-
-        mSearchHistory.add(keyword);
-
-        Account.inst().setSearchHistory(mSearchHistory);
-
-        updateData();
-    }
-
-    private void updateData() {
-        List<Map<String, String>> data = new ArrayList<Map<String, String>>();
-
-        final String[] from = new String[]{"search"};
-        final int[] to = new int[]{R.id.tv_name};
-
-        for (String search : mSearchHistory) {
-            Map<String, String> item = new HashMap<String, String>();
-            item.put(from[0], search);
-
-            data.add(item);
-        }
-
-        mLvHistory.setAdapter(new SimpleAdapter(this, data, R.layout.item_circle_simple, from, to));
-
-        if (mSearchHistory.size() > 0) {
-            mFooterClearSearch.setVisibility(View.VISIBLE);
-        } else {
-            mFooterClearSearch.setVisibility(View.GONE);
-        }
-    }
-
-    private void clearHistory() {
-        mSearchHistory.clear();
-        Account.inst().setSearchHistory(mSearchHistory);
-
-        updateData();
-    }
-
-    private void showHistory() {
-        mLvResult.setVisibility(View.GONE);
-        mLvHistory.setVisibility(View.VISIBLE);
-    }
-
-    private void showSearchResult() {
-        mLvResult.setVisibility(View.VISIBLE);
-        mLvHistory.setVisibility(View.GONE);
-    }
-
-    private void requestSearch(int page, String keyword) {
-        request(new SearchCircleRequest(page, keyword), new OnResponse<CirclesResponse>() {
-
-            @Override
-            public void onResponse(CirclesResponse response) {
-                if (response != null) {
-
-                }
-                hideProgressBar();
-            }
-        }, CirclesResponse.class);
-        showSearchResult();
-        showProgressBar();
-    }
+  }
 }
