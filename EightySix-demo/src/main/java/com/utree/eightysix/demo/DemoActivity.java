0ed9d0e615a5f6d5144f7986625b7b0b4b252add
Simon Yu <ccl1115@gmail.com>
2014-06-22 22:07:18 +0800

    remove demo codes to EightySix-demo which is not working now

--- EightySix-demo/src/main/java/com/utree/eightysix/demo/DemoActivity.java
+++ EightySix-demo/src/main/java/com/utree/eightysix/demo/DemoActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import com.utree.eightysix.R;
+import com.utree.eightysix.demo.R;
 import com.utree.eightysix.app.BaseActivity;
 import com.utree.eightysix.widget.TopBar;
 
