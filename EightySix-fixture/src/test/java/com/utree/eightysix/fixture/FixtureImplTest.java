03db6ab294270897665cc2c19ead1b342f790437
Simon Yu <ccl1115@gmail.com>
2014-06-22 13:42:11 +0800

    add test for fixture project

---
+++ EightySix-fixture/src/test/java/com/utree/eightysix/fixture/FixtureImplTest.java
@@ -0,0 +1,17 @@
+package com.utree.eightysix.fixture;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FixtureImplTest {
+
+  @Test
+  public void testGet() throws Exception {
+
+  }
+
+  @Test
+  public void testGet1() throws Exception {
+
+  }
+}
