d5f5852a92e868408c080cc15c37fee62d5a28d4
Simon Yu <ccl1115@gmail.com>
2014-07-18 22:50:55 +0800

    correct wrong files move

---
+++ EightySix-app/src/main/java/com/utree/eightysix/app/share/ShareManager.java
@@ -0,0 +1,124 @@
+package com.utree.eightysix.app.share.share;
+
+import android.app.Activity;
+import android.view.View;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.utree.eightysix.R;
+import com.utree.eightysix.annotations.Keep;
+import com.utree.eightysix.data.Post;
+import com.utree.eightysix.widget.ThemedDialog;
+
+/**
+ * @author simon
+ */
+public class ShareManager {
+
+
+  private IShare mShareToQQ = new ShareToQQ();
+  private IShare mShareViaSMS = new ShareViaSMS();
+
+  public ThemedDialog shareAppDialog(final Activity activity, int factoryId) {
+    ThemedDialog dialog;
+    dialog = new ThemedDialog(activity);
+    dialog.setTitle("分享给厂里的朋友");
+    dialog.setCanceledOnTouchOutside(true);
+    View view = activity.getLayoutInflater().inflate(R.layout.dialog_content_share, null, false);
+    dialog.setContent(view);
+    new ShareAppViewHolder(activity, view, factoryId);
+    dialog.show();
+    return dialog;
+  }
+
+  public ThemedDialog sharePostDialog(final Activity activity, Post post) {
+    ThemedDialog dialog;
+    dialog = new ThemedDialog(activity);
+    dialog.setTitle("分享给厂里的朋友");
+    dialog.setCanceledOnTouchOutside(true);
+    View view = activity.getLayoutInflater().inflate(R.layout.dialog_content_share, null, false);
+    dialog.setContent(view);
+    new SharePostViewHolder(activity, view, post);
+    dialog.show();
+    return dialog;
+  }
+
+  public ThemedDialog shareCommentDialog(final Activity activity, Post post, String comment) {
+    ThemedDialog dialog;
+    dialog = new ThemedDialog(activity);
+    dialog.setTitle("分享给厂里的朋友");
+    dialog.setCanceledOnTouchOutside(true);
+    View view = activity.getLayoutInflater().inflate(R.layout.dialog_content_share, null, false);
+    dialog.setContent(view);
+    new ShareCommentViewHolder(activity, view, post, comment);
+    dialog.show();
+    return dialog;
+  }
+
+  @Keep
+  public class ShareCommentViewHolder {
+    private Activity mActivity;
+    private Post mPost;
+    private String mComment;
+
+    ShareCommentViewHolder(Activity activity, View view, Post post, String comment) {
+      mActivity = activity;
+      mPost = post;
+      mComment = comment;
+      ButterKnife.inject(this, view);
+    }
+
+    @OnClick (R.id.tv_sms)
+    void onTvSmsClicked() {
+      mShareViaSMS.shareComment(mActivity, mPost, mComment);
+    }
+
+    @OnClick (R.id.tv_qq_friends)
+    void onQQFriendsClicked() {
+      mShareToQQ.shareComment(mActivity, mPost, mComment);
+    }
+  }
+
+  @Keep
+  public class SharePostViewHolder {
+    private Activity mActivity;
+    private Post mPost;
+
+    SharePostViewHolder(Activity activity, View view, Post post) {
+      mActivity = activity;
+      mPost = post;
+      ButterKnife.inject(this, view);
+    }
+
+    @OnClick (R.id.tv_sms)
+    void onTvSmsClicked() {
+      mShareViaSMS.sharePost(mActivity, mPost);
+    }
+
+    @OnClick (R.id.tv_qq_friends)
+    void onQQFriendsClicked() {
+      mShareToQQ.sharePost(mActivity, mPost);
+    }
+  }
+
+  @Keep
+  public class ShareAppViewHolder {
+    private Activity mActivity;
+    private int mFactoryId;
+
+    ShareAppViewHolder(Activity activity, View view, int factoryId) {
+      mActivity = activity;
+      mFactoryId = factoryId;
+      ButterKnife.inject(this, view);
+    }
+
+    @OnClick (R.id.tv_sms)
+    void onTvSmsClicked() {
+      mShareViaSMS.shareApp(mActivity, mFactoryId);
+    }
+
+    @OnClick (R.id.tv_qq_friends)
+    void onQQFriendsClicked() {
+      mShareToQQ.shareApp(mActivity, mFactoryId);
+    }
+  }
+}
---
+++ EightySix-app/src/main/java/com/utree/eightysix/app/share/ShareToQQ.java
@@ -0,0 +1,80 @@
+package com.utree.eightysix.app.share.share;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.tencent.connect.share.QQShare;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+import com.utree.eightysix.BuildConfig;
+import com.utree.eightysix.R;
+import com.utree.eightysix.U;
+import com.utree.eightysix.data.Post;
+
+/**
+ * @author simon
+ */
+class ShareToQQ extends IShare {
+
+  private Tencent sTencent =
+      Tencent.createInstance(U.getConfig("qq.app_id"), U.getContext().getApplicationContext());
+
+  private void shareToQQ(Activity from, Bundle data, IUiListener listener) {
+    data.putString(QQShare.SHARE_TO_QQ_APP_NAME, U.getContext().getString(R.string.app_name));
+    sTencent.shareToQQ(from, data, listener);
+  }
+
+  @Override
+  public void shareApp(Activity activity, int circleId) {
+    Bundle data = new Bundle();
+    data.putString(QQShare.SHARE_TO_QQ_TITLE, shareTitleForApp());
+    data.putString(QQShare.SHARE_TO_QQ_SUMMARY, shareContentForApp());
+    data.putString(QQShare.SHARE_TO_QQ_TARGET_URL, shareLinkForApp(circleId));
+    data.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+    shareToQQ(activity, data, defaultListener());
+  }
+
+  @Override
+  public void sharePost(Activity activity, Post post) {
+    Bundle data = new Bundle();
+    data.putString(QQShare.SHARE_TO_QQ_TITLE, shareTitleForPost());
+    data.putString(QQShare.SHARE_TO_QQ_SUMMARY, post.content);
+    data.putString(QQShare.SHARE_TO_QQ_TARGET_URL, shareLinkForPost(post.id));
+    data.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+    shareToQQ(activity, data, defaultListener());
+  }
+
+  @Override
+  public void shareComment(Activity activity, Post post, String comment) {
+    Bundle data = new Bundle();
+    data.putString(QQShare.SHARE_TO_QQ_TITLE, shareTitleForComment());
+    data.putString(QQShare.SHARE_TO_QQ_SUMMARY, comment);
+    data.putString(QQShare.SHARE_TO_QQ_TARGET_URL, shareLinkForComment(post.id));
+    data.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+    shareToQQ(activity, data, defaultListener());
+  }
+
+  private IUiListener defaultListener() {
+    return new IUiListener() {
+      @Override
+      public void onComplete(Object o) {
+        if (BuildConfig.DEBUG) Toast.makeText(U.getContext(), "onComplete", Toast.LENGTH_LONG).show();
+      }
+
+      @Override
+      public void onError(UiError uiError) {
+        if (BuildConfig.DEBUG)
+          Toast.makeText(U.getContext(),
+              String.format("%d: %s - %s", uiError.errorCode, uiError.errorMessage, uiError.errorDetail),
+              Toast.LENGTH_LONG).show();
+      }
+
+      @Override
+      public void onCancel() {
+        if (BuildConfig.DEBUG) Toast.makeText(U.getContext(), "onCancel", Toast.LENGTH_LONG).show();
+      }
+    };
+  }
+
+}
