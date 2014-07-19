757dca432667e9f6a634453bb839bd5da8bd19f7
Simon Yu <ccl1115@gmail.com>
2014-07-19 20:31:31 +0800

    add sync request

--- EightySix-app/src/main/java/com/utree/eightysix/C.java
+++ EightySix-app/src/main/java/com/utree/eightysix/C.java
@@ -57,6 +57,8 @@ public class C {
 
   public static final String API_PROMOTION = "/activity.do";
 
+  public static final String API_SYNC = "/system/sync.do";
+
 
   public static String IMEI = "";
 
