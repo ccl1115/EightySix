e939e48f2fba67613f641a9dc2db6b0d22b7e5a9
Simon Yu <ccl1115@gmail.com>
2014-10-21 20:05:56 +0800

    增加IP访问接口机制 修复ping方法没有正确调用的问题

--- EightySix-app/src/main/java/com/utree/eightysix/utils/PingService.java
+++ EightySix-app/src/main/java/com/utree/eightysix/utils/PingService.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.os.IBinder;
 import com.utree.eightysix.U;
 import com.utree.eightysix.rest.RESTRequester;
+import de.akquinet.android.androlog.Log;
 import java.io.IOException;
 
 /**
  * @author simon
  */
 public class PingService extends Service {
+
+  private static final String TAG = "PingService";
 
   @Override
   public IBinder onBind(Intent intent) {
@@ -20,20 +23,26 @@ public class PingService extends Service {
   @Override
   public int onStartCommand(Intent intent, int flags, int startId) {
     try {
-      Process process = new ProcessBuilder("ping", U.getConfig("api.host").replace("http://", ""), "-c 3").start();
+      ProcessBuilder ping = new ProcessBuilder("ping", "-c 3", U.getConfig("api.host").replace("http://", "").split(":")[0]);
+      Process process = ping.start();
       if (process.waitFor() == 0) {
+        Log.d(TAG, "ping success");
         U.getRESTRequester().setHost(U.getConfig("api.host"));
       } else {
         if (Env.getHostIp() != null) {
+          Log.d(TAG, "ping failed, use sync ip");
           U.getRESTRequester().setHost("http://" + Env.getHostIp());
         } else {
+          Log.d(TAG, "ping failed, no sync ip");
           U.getAnalyser().reportError(this, "PingService: failed to update host, not get ip from sync api");
         }
 
       }
-    } catch (IOException ignored) {
+    } catch (IOException e) {
+      Log.d(TAG, "ping failed, io exception");
       U.getAnalyser().reportError(this, "PingService: failed to ping host, IOException");
-    } catch (InterruptedException ignored) {
+    } catch (InterruptedException e) {
+      Log.d(TAG, "ping failed, interrupted exception");
       U.getAnalyser().reportError(this, "PingService: failed to ping host, InterruptedException");
     }
     return START_NOT_STICKY;
