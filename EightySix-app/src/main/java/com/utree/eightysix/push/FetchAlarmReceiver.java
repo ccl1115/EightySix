939173d31e9182c240d3e15619defbe4d856fa3f
Simon Yu <ccl1115@gmail.com>
2014-10-23 18:40:01 +0800

    Log fetch history in release version

--- EightySix-app/src/main/java/com/utree/eightysix/push/FetchAlarmReceiver.java
+++ EightySix-app/src/main/java/com/utree/eightysix/push/FetchAlarmReceiver.java
@@ -5,11 +5,8 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import com.utree.eightysix.BuildConfig;
 import com.utree.eightysix.app.msg.FetchNotificationService;
 import com.utree.eightysix.utils.IOUtils;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,28 +22,24 @@ public class FetchAlarmReceiver extends BroadcastReceiver {
   private static final int INTERVAL = 3600000;
 
   public static void setupAlarm(Context context) {
-    Log.d(TAG, "setupAlarm");
     AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
     alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + INTERVAL, INTERVAL,
         PendingIntent.getBroadcast(context, 0, new Intent(ACTION), PendingIntent.FLAG_UPDATE_CURRENT));
 
-    if (BuildConfig.DEBUG) {
-      File f = IOUtils.createTmpFile("fetch_log");
+    File f = IOUtils.createTmpFile("fetch_log");
 
-      FileWriter wf = null;
-      try {
-        wf = new FileWriter(f, true);
-        wf.write('\n');
-        wf.write("setup alarm at " + new Date().toString());
-        wf.write('\n');
-        wf.flush();
-      } catch (IOException e) {
-        e.printStackTrace();
-        if (wf != null) {
-          try {
-            wf.close();
-          } catch (IOException ignored) {
-          }
+    FileWriter wf = null;
+    try {
+      wf = new FileWriter(f, true);
+      wf.write("setup alarm at " + new Date().toString());
+      wf.write('\n');
+      wf.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
+      if (wf != null) {
+        try {
+          wf.close();
+        } catch (IOException ignored) {
         }
       }
     }
@@ -55,26 +48,22 @@ public class FetchAlarmReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
     if (ACTION.equals(intent.getAction())) {
-      Log.d(TAG, "onReceive");
       context.startService(new Intent(context, FetchNotificationService.class));
 
-      if (BuildConfig.DEBUG) {
-        File f = IOUtils.createTmpFile("fetch_log");
+      File f = IOUtils.createTmpFile("fetch_log");
 
-        FileWriter wf = null;
-        try {
-          wf = new FileWriter(f, true);
-          wf.write('\n');
-          wf.write("start fetch service at " + new Date().toString());
-          wf.write('\n');
-          wf.flush();
-        } catch (IOException e) {
-          e.printStackTrace();
-          if (wf != null) {
-            try {
-              wf.close();
-            } catch (IOException ignored) {
-            }
+      FileWriter wf = null;
+      try {
+        wf = new FileWriter(f, true);
+        wf.write("start fetch service at " + new Date().toString());
+        wf.write('\n');
+        wf.flush();
+      } catch (IOException e) {
+        e.printStackTrace();
+        if (wf != null) {
+          try {
+            wf.close();
+          } catch (IOException ignored) {
           }
         }
       }
