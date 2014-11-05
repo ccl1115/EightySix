03389b41b0187e641fd7c4f1b23b3f46d533d060
Simon Yu <ccl1115@gmail.com>
2014-11-05 21:07:54 +0800

    [tag] restore tag from bundle when activity restore

--- EightySix-app/src/main/java/com/utree/eightysix/app/tag/AbsTagFragment.java
+++ EightySix-app/src/main/java/com/utree/eightysix/app/tag/AbsTagFragment.java
@@ -2,6 +2,7 @@ package com.utree.eightysix.app.tag;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,6 +60,21 @@ public abstract class AbsTagFragment extends BaseFragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_feed, container, false);
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    outState.putParcelable("tag", mTag);
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override
+  public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+    super.onViewStateRestored(savedInstanceState);
+
+    if (savedInstanceState != null) {
+      mTag = savedInstanceState.getParcelable("tag");
+    }
   }
 
   @Override
