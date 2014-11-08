4aa97806554baeb9705a13c8dd0dbf63a4402df8
simon <bb.simon.yu@gmail.com>
2014-11-08 14:58:17 +0800

    Bug 647 - 【分享】分享内容选择菜单，点击空间无反应

--- EightySix-app/src/main/java/com/utree/eightysix/app/share/ShareToQzone.java
+++ EightySix-app/src/main/java/com/utree/eightysix/app/share/ShareToQzone.java
@@ -76,10 +76,13 @@ class ShareToQzone extends IShare {
         data.putString(QzoneShare.SHARE_TO_QQ_TITLE, String.format(shareTitleForPost(), post.shortName));
         data.putString(QzoneShare.SHARE_TO_QQ_TARGET_URL, url);
         data.putString(QzoneShare.SHARE_TO_QQ_SUMMARY, post.content);
+        ArrayList<String> urls = new ArrayList<String>();
         if (!TextUtils.isEmpty(post.bgUrl) && post.bgUrl.contains(U.getImageBucket())) {
-          data.putString(QzoneShare.SHARE_TO_QQ_IMAGE_URL, post.bgUrl);
+          urls.add(post.bgUrl);
+          data.putStringArrayList(QzoneShare.SHARE_TO_QQ_IMAGE_URL, urls);
         } else {
-          data.putString(QzoneShare.SHARE_TO_QQ_IMAGE_URL, "http://utree-resource.oss-cn-beijing.aliyuncs.com/faceless.png");
+          urls.add("http://utree-resource.oss-cn-beijing.aliyuncs.com/faceless.png");
+          data.putStringArrayList(QzoneShare.SHARE_TO_QQ_IMAGE_URL, urls);
         }
         data.putInt(QzoneShare.SHARE_TO_QZONE_KEY_TYPE, QzoneShare.SHARE_TO_QZONE_TYPE_IMAGE_TEXT);
         shareToQzone(activity, data, postUiCallback(post));
@@ -108,10 +111,13 @@ class ShareToQzone extends IShare {
         data.putString(QzoneShare.SHARE_TO_QQ_TITLE, shareTitleForComment());
         data.putString(QzoneShare.SHARE_TO_QQ_SUMMARY, String.format("\"%s\"", comment));
         data.putString(QzoneShare.SHARE_TO_QQ_TARGET_URL, url);
+        ArrayList<String> urls = new ArrayList<String>();
         if (!TextUtils.isEmpty(post.bgUrl) && post.bgUrl.contains(U.getImageBucket())) {
-          data.putString(QzoneShare.SHARE_TO_QQ_IMAGE_URL, post.bgUrl);
+          urls.add(post.bgUrl);
+          data.putStringArrayList(QzoneShare.SHARE_TO_QQ_IMAGE_URL, urls);
         } else {
-          data.putString(QzoneShare.SHARE_TO_QQ_IMAGE_URL, "http://utree-resource.oss-cn-beijing.aliyuncs.com/faceless.png");
+          urls.add("http://utree-resource.oss-cn-beijing.aliyuncs.com/faceless.png");
+          data.putStringArrayList(QzoneShare.SHARE_TO_QQ_IMAGE_URL, urls);
         }
         data.putInt(QzoneShare.SHARE_TO_QZONE_KEY_TYPE, QzoneShare.SHARE_TO_QZONE_TYPE_IMAGE_TEXT);
         shareToQzone(activity, data, new BaseUiListener());
