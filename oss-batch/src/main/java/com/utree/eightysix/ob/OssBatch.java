98243c8944681945b002c7d3e4fd42f95357e8b2
simon <bb.simon.yu@gmail.com>
2014-11-12 22:28:58 +0800

    oss batch works, to be test

--- oss-batch/src/main/java/com/utree/eightysix/ob/OssBatch.java
+++ oss-batch/src/main/java/com/utree/eightysix/ob/OssBatch.java
@@ -2,11 +2,13 @@ package com.utree.eightysix.ob;
 
 import com.aliyun.openservices.oss.OSSClient;
 import com.aliyun.openservices.oss.model.*;
-import org.apache.commons.io.FileUtils;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.common.io.Files;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.charset.Charset;
+import java.util.*;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -41,71 +43,128 @@ public class OssBatch {
   Logger mLogger;
 
   List<String> mOverride;
+  private List<String> mRemoteList;
 
   OSSClient mOSSClient =
       new OSSClient(C.ENDPOINT, C.ACCESS_KEY_ID, C.ACCESS_KEY_SECRET);
 
   public OssBatch() {
     mLogger = Logger.getLogger("oss-batch");
+    mLogger.setLevel(Level.ALL);
     try {
       mLogger.addHandler(new FileHandler("log"));
     } catch (IOException e) {
+      e.printStackTrace();
       System.exit(1);
     }
+
+    readLocalOverride();
+    readRemoteObjectsList();
   }
 
   private void readLocalOverride() {
     mOverride = new ArrayList<String>();
     try {
-      mOverride = FileUtils.readLines(new File("override"));
+      mOverride = Files.readLines(new File("override"), Charset.defaultCharset());
+    } catch (FileNotFoundException ignored) {
     } catch (IOException e) {
+      e.printStackTrace();
       System.exit(1);
     }
+    mLogger.log(Level.INFO, String.format("get %d items from local", mOverride.size()));
   }
 
-  private void readRemoteList() {
-    ListObjectsRequest request = new ListObjectsRequest(C.BUCKET, "", "", "", 500);
+  private void readRemoteObjectsList() {
+    mRemoteList = new ArrayList<String>();
+    String marker = "";
+    do {
+      marker = requestPage(marker);
+      mLogger.log(Level.INFO, "marker: " + marker);
+      try {
+        synchronized (this) {
+          wait(500);
+        }
+      } catch (InterruptedException ignored) {
+      }
+    } while(marker != null);
+    mLogger.log(Level.INFO, String.format("get %d items from remote", mRemoteList.size()));
   }
 
-  public void batch() {
-
-    ObjectMetadata objectMetadata = new ObjectMetadata();
-    objectMetadata.setContentType("image/jpg");
-
-    for (String p : PREFIX) {
-      for (String s : SUB_PREFIX) {
-        mLogger.log(Level.INFO, String.format("start get list with prefix: /%s/%s", p, s));
-        ObjectListing objectListing = mOSSClient.listObjects(C.BUCKET, p + "/" + s);
-        List<OSSObjectSummary> objectSummaries = objectListing.getObjectSummaries();
-        mLogger.log(Level.INFO, String.format("get %d objects with prefix /%s/%s", objectSummaries.size(), p, s));
-
-        for (OSSObjectSummary summary : objectSummaries) {
-          CopyObjectRequest r = new CopyObjectRequest(C.BUCKET, summary.getKey(), C.BUCKET, summary.getKey());
-          r.setNewObjectMetadata(objectMetadata);
-
-          mOSSClient.copyObject(r);
-
-          mLogger.log(Level.INFO, String.format("object %s is updated", summary.getKey()));
-
-          try {
-            synchronized (this) {
-              wait(500);
-            }
-          } catch (InterruptedException ignored) {
-          }
-        }
-
-        try {
-          synchronized (this) {
-            wait(2000);
-          }
-        } catch (InterruptedException ignored) {
-        }
+  private String requestPage(String marker) {
+    ListObjectsRequest request = new ListObjectsRequest(C.BUCKET, "", marker, "", 300);
+    ObjectListing objectListing = mOSSClient.listObjects(request);
+    List<OSSObjectSummary> objectSummaries = objectListing.getObjectSummaries();
+    if (objectSummaries.size() == 0) {
+      mLogger.log(Level.INFO, String.format("get %d item from page", 0));
+      return null;
+    } else {
+      for (OSSObjectSummary summary : objectSummaries) {
+        mRemoteList.add(summary.getKey());
+      }
+      mLogger.log(Level.INFO, String.format("get %d item from page", objectSummaries.size()));
+      if (objectListing.isTruncated()) {
+        return null;
+      } else {
+        return objectListing.getNextMarker();
       }
     }
   }
 
-  public void update(OSSObject ossObject) {
+  public void batch() {
+    TreeSet<String> set1 = new TreeSet<String>(mOverride);
+    TreeSet<String> set2 = new TreeSet<String>(mRemoteList);
+
+    mLogger.log(Level.INFO, String.format("%d items in local", set1.size()));
+    mLogger.log(Level.INFO, String.format("%d items in remote", set2.size()));
+
+    Set<String> toBeConvert = Sets.difference(set2, set1);
+
+    mLogger.log(Level.INFO, String.format("%d items to be update", toBeConvert.size()));
+
+    ObjectMetadata objectMetadata = new ObjectMetadata();
+    objectMetadata.setContentType("image/jpg");
+
+    int index = 0;
+    for (String s : toBeConvert) {
+      if (index > 50) break;
+      CopyObjectRequest request = new CopyObjectRequest(C.BUCKET, s, C.BUCKET, s);
+      request.setNewObjectMetadata(objectMetadata);
+
+      mOSSClient.copyObject(request);
+      mOverride.add(s);
+
+      mLogger.log(Level.INFO, String.format("updated %s", s));
+
+      index ++;
+
+      try {
+        synchronized (this) {
+          wait(500);
+        }
+      } catch (InterruptedException ignored) {
+      }
+    }
+
+    BufferedWriter override = null;
+    try {
+      override = Files.newWriter(new File("override"), Charset.defaultCharset());
+      for (String item : mOverride) {
+        override.write(item);
+        override.write('\n');
+      }
+
+    } catch (FileNotFoundException e) {
+      System.exit(-1);
+    } catch (IOException ignored) {
+    } finally {
+      if (override != null) {
+        try {
+          override.flush();
+          override.close();
+        } catch (IOException ignored) {
+        }
+      }
+    }
   }
 
   public static int main(String[] args) {
