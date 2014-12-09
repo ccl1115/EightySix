5e7c09de2d07de52fe3c102ac31aaf73c342412e
yulu <yulu@utree.cn>
2014-12-09 15:23:38 +0800

    conversation item layout

--- EightySix-app/src/main/java/com/utree/eightysix/app/chat/ConversationActivity.java
+++ EightySix-app/src/main/java/com/utree/eightysix/app/chat/ConversationActivity.java
@@ -8,15 +8,26 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import butterknife.InjectView;
 import com.utree.eightysix.Account;
 import com.utree.eightysix.R;
 import com.utree.eightysix.app.BaseActivity;
 import com.utree.eightysix.app.Layout;
+import com.utree.eightysix.app.TopTitle;
+import com.utree.eightysix.view.SwipeRefreshLayout;
+import com.utree.eightysix.widget.AdvancedListView;
 
 /**
  */
 @Layout(R.layout.activity_conversation)
+@TopTitle(R.string.chat_anonymous)
 public class ConversationActivity extends BaseActivity {
+
+  @InjectView(R.id.alv_conversation)
+  public AdvancedListView mAlvConversation;
+
+  @InjectView(R.id.content)
+  public SwipeRefreshLayout mSwipeRefreshLayout;
 
   public static void start(Context context) {
     Intent intent = new Intent(context, ConversationActivity.class);
@@ -31,6 +42,25 @@ public class ConversationActivity extends BaseActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    mAlvConversation.setAdapter(new ConversationAdapter());
+
+    mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+      @Override
+      public void onRefresh() {
+
+      }
+
+      @Override
+      public void onDrag() {
+
+      }
+
+      @Override
+      public void onCancel() {
+
+      }
+    });
   }
 
   @Override
--- EightySix-app/src/main/java/com/utree/eightysix/app/chat/ConversationAdapter.java
+++ EightySix-app/src/main/java/com/utree/eightysix/app/chat/ConversationAdapter.java
@@ -4,16 +4,27 @@
 
 package com.utree.eightysix.app.chat;
 
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.utree.eightysix.R;
+import com.utree.eightysix.U;
+import com.utree.eightysix.drawable.RoundRectDrawable;
+import com.utree.eightysix.utils.ColorUtil;
+import com.utree.eightysix.widget.AsyncImageView;
+import com.utree.eightysix.widget.FontPortraitView;
 
 /**
  */
 public class ConversationAdapter extends BaseAdapter {
   @Override
   public int getCount() {
-    return 0;
+    return 10;
   }
 
   @Override
@@ -27,7 +38,61 @@ public class ConversationAdapter extends BaseAdapter {
   }
 
   @Override
-  public View getView(int i, View view, ViewGroup viewGroup) {
-    return null;
+  public View getView(int position, View convertView, ViewGroup parent) {
+    ViewHolder holder;
+    if (convertView == null) {
+      convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_conversation, parent, false);
+      holder = new ViewHolder(convertView);
+      convertView.setTag(holder);
+    } else {
+      holder = (ViewHolder) convertView.getTag();
+    }
+
+    holder.mTvName.setText("陌生人");
+    holder.mTvStatus.setText("在线");
+    holder.mTvCircle.setText("仁宝电脑");
+    holder.mTvLast.setText("哈哈哈哈哈哈");
+    holder.mFpvPortrait.setText("\ue800");
+    holder.mTvContent.setText("我是个大鸡巴");
+    holder.mTvTime.setText("刚刚");
+
+    holder.mFpvPortrait.setTextColor(Color.BLUE);
+    holder.mFpvPortrait.setBackgroundDrawable(new RoundRectDrawable(Integer.MAX_VALUE, ColorUtil.lighten(Color.BLUE)));
+
+    holder.mTvStatus.setBackgroundDrawable(new RoundRectDrawable(U.dp2px(2), Color.GREEN));
+
+    return convertView;
+  }
+
+  public static class ViewHolder {
+
+    @InjectView(R.id.tv_name)
+    public TextView mTvName;
+
+    @InjectView(R.id.tv_time)
+    public TextView mTvTime;
+
+    @InjectView(R.id.tv_circle)
+    public TextView mTvCircle;
+
+    @InjectView(R.id.tv_status)
+    public TextView mTvStatus;
+
+    @InjectView(R.id.tv_last)
+    public TextView mTvLast;
+
+    @InjectView(R.id.fpv_portrait)
+    public FontPortraitView mFpvPortrait;
+
+    @InjectView(R.id.tv_content)
+    public TextView mTvContent;
+
+    @InjectView(R.id.aiv_post_bg)
+    public AsyncImageView mAivPostBg;
+
+
+    public ViewHolder(View view) {
+      ButterKnife.inject(this, view);
+    }
   }
 }
