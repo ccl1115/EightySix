2a409af7cb7a33848731c729073b73fde4ca0834
yulu <yulu@utree.cn>
2015-01-22 23:25:11 +0800

    remove clear cache in main settings

--- app/src/main/java/com/utree/eightysix/app/settings/MainSettingsActivity.java
+++ app/src/main/java/com/utree/eightysix/app/settings/MainSettingsActivity.java
@@ -3,7 +3,6 @@ package com.utree.eightysix.app.settings;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -20,8 +19,6 @@ import com.utree.eightysix.app.devmode.DevModeActivity;
 import com.utree.eightysix.data.Sync;
 import com.utree.eightysix.widget.RoundedButton;
 
-import java.io.IOException;
-
 /**
  * @author simon
  */
@@ -37,9 +34,6 @@ public class MainSettingsActivity extends BaseActivity {
 
   @InjectView(R.id.tv_dev)
   public TextView mTvDev;
-
-  @InjectView(R.id.tv_cache_size)
-  public TextView mTvCacheSize;
 
   @OnClick (R.id.tv_logout)
   public void onRbLogoutClicked() {
@@ -86,12 +80,6 @@ public class MainSettingsActivity extends BaseActivity {
     startActivity(new Intent(this, DevModeActivity.class));
   }
 
-  @OnClick(R.id.ll_clear_cache)
-  public void onLlClearCacheClicked() {
-    showProgressBar(true);
-    new ClearCacheWorker().execute();
-  }
-
   @OnCheckedChanged(R.id.cb_silent_mode)
   public void onCbSilentModeChecked(boolean checked){
     Account.inst().setSilentMode(checked);
@@ -125,8 +113,6 @@ public class MainSettingsActivity extends BaseActivity {
     }
 
     mCbSilentMode.setChecked(Account.inst().getSilentMode());
-
-    new CacheSizeWorker().execute();
   }
 
   @Override
@@ -135,55 +121,4 @@ public class MainSettingsActivity extends BaseActivity {
     finish();
   }
 
-  public class CacheSizeWorker extends AsyncTask<Void, Void, Long> {
-
-    @Override
-    protected Long doInBackground(Void... voids) {
-      return U.getImageCache().size() + U.getApiCache().size() + U.getContactsCache().size();
-    }
-
-    @Override
-    protected void onPostExecute(Long aLong) {
-      String sizeInHuman;
-
-      int mb = 1024 * 1024;
-      if (aLong > mb) {
-
-        sizeInHuman = String.valueOf(aLong / mb);
-        sizeInHuman += ".";
-        sizeInHuman += String.valueOf(aLong % mb).substring(0, 2) + "MB";
-      } else {
-        sizeInHuman = (aLong / 1024) + "KB";
-      }
-
-      mTvCacheSize.setText("目前缓存大小：" + sizeInHuman);
-    }
-  }
-
-  public class ClearCacheWorker extends AsyncTask<Void, Void, Void> {
-
-    @Override
-    protected Void doInBackground(Void... voids) {
-      try {
-        U.getApiCache().delete();
-        U.getApiCache().flush();
-        U.getImageCache().delete();
-        U.getImageCache().flush();
-        U.getContactsCache().delete();
-        U.getContactsCache().flush();
-      } catch (IOException ignored) {
-        if (BuildConfig.DEBUG) {
-          ignored.printStackTrace();
-        }
-      }
-      return null;
-    }
-
-    @Override
-    protected void onPostExecute(Void aVoid) {
-      mTvCacheSize.setText("目前缓存大小：0KB");
-      showToast(getString(R.string.clear_cache_succeed));
-      hideProgressBar();
-    }
-  }
 }
