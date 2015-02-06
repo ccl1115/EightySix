ed37e0db9d4f318f7af219ad6c0af63a4d6faa79
yulu <yulu@utree.cn>
2015-02-06 19:37:56 +0800

    Bug 885 - 【拜年卡】当祝福语输入框中的内容被修改后，点击换一换，应按如下策略

--- app/src/main/java/com/utree/eightysix/app/feed/FeedBainianView.java
+++ app/src/main/java/com/utree/eightysix/app/feed/FeedBainianView.java
@@ -8,6 +8,7 @@ package com.utree.eightysix.app.feed;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -15,10 +16,12 @@ import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 import com.squareup.picasso.Picasso;
 import com.utree.eightysix.R;
 import com.utree.eightysix.U;
 import com.utree.eightysix.data.Bainian;
+import com.utree.eightysix.widget.ThemedDialog;
 
 /**
  */
@@ -36,17 +39,21 @@ public final class FeedBainianView extends FrameLayout {
   @InjectView(R.id.tv_sub_title)
   public TextView mTvSubTitle;
 
-  @InjectView(R.id.tv_tip)
-  public TextView mTvTip;
-
   @InjectView(R.id.rb_generate)
   public TextView mRbGenerate;
 
   @InjectView(R.id.iv_bg)
   public ImageView mIvBg;
 
+  private boolean mTextChanged;
+
   private int mIndex;
   private Bainian mBainian;
+
+  @OnTextChanged(R.id.et_msg)
+  public void onEtMsgTextChanged(CharSequence cs) {
+    mTextChanged = true;
+  }
 
   public FeedBainianView(Context context) {
     this(context, null, 0);
@@ -70,19 +77,58 @@ public final class FeedBainianView extends FrameLayout {
     mEtRecipient.setHint(mBainian.receiveText);
     mTvTitle.setText(mBainian.title);
     mTvSubTitle.setText(mBainian.subTitle);
-    mTvTip.setText(mBainian.receiveText);
     mRbGenerate.setText(mBainian.buttonText);
     mEtMsg.setText(mBainian.newYearContents.get(mIndex).content);
   }
 
   @OnClick(R.id.iv_refresh)
   public void onIvRefreshClicked() {
-    mIndex = mIndex >= mBainian.newYearContents.size() - 1 ? 0 : mIndex + 1;
-    mEtMsg.setText(mBainian.newYearContents.get(mIndex).content);
+    if (mTextChanged) {
+      showChangeTextDialog();
+    } else {
+      changeText();
+    }
   }
 
   @Override
   protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
     super.onMeasure(widthMeasureSpec, widthMeasureSpec - U.dp2px(8));
+  }
+
+  private void showChangeTextDialog() {
+    final ThemedDialog dialog = new ThemedDialog(getContext());
+
+    dialog.setTitle("提醒");
+
+    TextView view = new TextView(getContext());
+
+    view.setText("换一换后，会删除掉之前编辑的祝福语哦！");
+    int p = U.dp2px(16);
+    view.setPadding(p, p, p, p);
+
+    dialog.setContent(view);
+
+    dialog.setPositive("换一换", new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        changeText();
+        dialog.dismiss();
+      }
+    });
+
+    dialog.setRbNegative("取消", new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        dialog.dismiss();
+      }
+    });
+
+    dialog.show();
+  }
+
+  public void changeText() {
+    mIndex = mIndex >= mBainian.newYearContents.size() - 1 ? 0 : mIndex + 1;
+    mEtMsg.setText(mBainian.newYearContents.get(mIndex).content);
+    mTextChanged = false;
   }
 }
