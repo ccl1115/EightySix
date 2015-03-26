8bea673dc6cb7f543c245d7b39bc49c9d2db3900
yulu <yulu@utree.cn>
2015-03-26 09:55:32 +0800

    FriendListActivity empty view

--- app/src/main/java/com/utree/eightysix/app/friends/FriendListActivity.java
+++ app/src/main/java/com/utree/eightysix/app/friends/FriendListActivity.java
@@ -5,6 +5,7 @@
 package com.utree.eightysix.app.friends;
 
 import android.os.Bundle;
+import android.view.View;
 import butterknife.InjectView;
 import com.squareup.otto.Subscribe;
 import com.utree.eightysix.Account;
@@ -17,6 +18,7 @@ import com.utree.eightysix.response.FriendListResponse;
 import com.utree.eightysix.rest.OnResponse2;
 import com.utree.eightysix.rest.RESTRequester;
 import com.utree.eightysix.view.PinnedHeaderListView;
+import com.utree.eightysix.widget.RandomSceneTextView;
 
 /**
  */
@@ -24,8 +26,11 @@ import com.utree.eightysix.view.PinnedHeaderListView;
 @TopTitle(R.string.my_friends)
 public class FriendListActivity extends BaseActivity {
 
-  @InjectView(R.id.content)
+  @InjectView(R.id.plv_friends)
   public PinnedHeaderListView mAdvFriends;
+
+  @InjectView(R.id.rstv_empty)
+  public RandomSceneTextView mRstvEmpty;
 
   private FriendListAdapter mFriendListAdapter;
 
@@ -35,19 +40,28 @@ public class FriendListActivity extends BaseActivity {
 
     getTopBar().getAbLeft().setDrawable(getDrawable(R.drawable.top_bar_return));
 
+    mRstvEmpty.setDrawable(R.drawable.scene_4);
+    mRstvEmpty.setText("你还没有朋友");
+
+    showProgressBar(true);
     U.request("user_friend_list", new OnResponse2<FriendListResponse>() {
       @Override
       public void onResponseError(Throwable e) {
-
+        hideProgressBar();
       }
 
       @Override
       public void onResponse(FriendListResponse response) {
         if (RESTRequester.responseOk(response)) {
-          mFriendListAdapter = new FriendListAdapter(response.object);
-
-          mAdvFriends.setAdapter(mFriendListAdapter);
+          if (response.object.size() == 0) {
+            mRstvEmpty.setVisibility(View.VISIBLE);
+          } else {
+            mRstvEmpty.setVisibility(View.GONE);
+            mFriendListAdapter = new FriendListAdapter(response.object);
+            mAdvFriends.setAdapter(mFriendListAdapter);
+          }
         }
+        hideProgressBar();
       }
     }, FriendListResponse.class);
   }
