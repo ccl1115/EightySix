59dcfec6798d66ed1f02806f627e65903777434f
yulu <yulu@utree.cn>
2015-03-26 19:45:45 +0800

    search tags and keywords

--- app/src/main/java/com/utree/eightysix/app/feed/FeedsSearchActivity.java
+++ app/src/main/java/com/utree/eightysix/app/feed/FeedsSearchActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import butterknife.InjectView;
 import com.squareup.otto.Subscribe;
 import com.utree.eightysix.Account;
@@ -18,12 +19,19 @@ import com.utree.eightysix.U;
 import com.utree.eightysix.app.BaseActivity;
 import com.utree.eightysix.app.Layout;
 import com.utree.eightysix.data.Paginate;
+import com.utree.eightysix.data.Tag;
 import com.utree.eightysix.response.FeedsResponse;
+import com.utree.eightysix.response.TagsResponse;
 import com.utree.eightysix.rest.OnResponse2;
 import com.utree.eightysix.rest.RESTRequester;
+import com.utree.eightysix.view.RandomFloatingLayout;
 import com.utree.eightysix.widget.AdvancedListView;
 import com.utree.eightysix.widget.LoadMoreCallback;
 import com.utree.eightysix.widget.RandomSceneTextView;
+import com.utree.eightysix.widget.RoundedButton;
+
+import java.util.List;
+import java.util.Random;
 
 /**
  */
@@ -35,6 +43,9 @@ public class FeedsSearchActivity extends BaseActivity {
 
   @InjectView(R.id.rstv_empty)
   public RandomSceneTextView mRstvEmpty;
+
+  @InjectView(R.id.rfl_tags)
+  public RandomFloatingLayout mRflTags;
 
   private FeedsSearchAdapter mFeedsSearchAdapter;
 
@@ -92,10 +103,12 @@ public class FeedsSearchActivity extends BaseActivity {
 
       @Override
       public boolean onLoadMoreStart() {
-        requestFeeds(mPageInfo.currPage + 1);
+        requestFeedsSearch(mPageInfo.currPage + 1);
         return true;
       }
     });
+
+    requestTags();
   }
 
   @Override
@@ -113,10 +126,10 @@ public class FeedsSearchActivity extends BaseActivity {
   public void onActionSearchClicked(CharSequence cs) {
     hideSoftKeyboard(mAlvFeeds);
     mSearchContent = cs.toString();
-    requestFeeds(1);
+    requestFeedsSearch(1);
   }
 
-  public void requestFeeds(final int page) {
+  private void requestFeedsSearch(final int page) {
     showProgressBar(true);
     U.request("feeds_search", new OnResponse2<FeedsResponse>() {
       @Override
@@ -126,11 +139,8 @@ public class FeedsSearchActivity extends BaseActivity {
 
       @Override
       public void onResponse(FeedsResponse response) {
-
         if (RESTRequester.responseOk(response)) {
-
           if (page == 1) {
-
             if (response.object.posts.lists.size() == 0) {
               mRstvEmpty.setVisibility(View.VISIBLE);
               mAlvFeeds.setAdapter(null);
@@ -139,7 +149,6 @@ public class FeedsSearchActivity extends BaseActivity {
               mAlvFeeds.setAdapter(mFeedsSearchAdapter);
               mRstvEmpty.setVisibility(View.GONE);
             }
-
           } else {
             mFeedsSearchAdapter.add(response.object.posts.lists);
           }
@@ -150,5 +159,60 @@ public class FeedsSearchActivity extends BaseActivity {
         mAlvFeeds.stopLoadMore();
       }
     }, FeedsResponse.class, mSearchContent, null, mCurrent, page);
+  }
+
+  private void requestTags() {
+    showProgressBar(true);
+    U.request("tags_and_keywords", new OnResponse2<TagsResponse>() {
+      @Override
+      public void onResponseError(Throwable e) {
+        hideProgressBar();
+      }
+
+      @Override
+      public void onResponse(TagsResponse response) {
+        if (RESTRequester.responseOk(response)) {
+          mRflTags.setAdapter(new TagsAdapter(response.object.tags));
+        }
+        hideProgressBar();
+      }
+    }, TagsResponse.class);
+  }
+
+
+  public static class TagsAdapter extends BaseAdapter {
+
+    private List<Tag> mTags;
+    private Random mRandom = new Random();
+
+    public TagsAdapter(List<Tag> tags) {
+      mTags = tags;
+    }
+
+    @Override
+    public int getCount() {
+      return mTags.size();
+    }
+
+    @Override
+    public Tag getItem(int position) {
+      return mTags.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+      return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      RoundedButton btn = new RoundedButton(parent.getContext());
+
+      btn.setText(mTags.get(position).content);
+      btn.setTextSize(10 + mRandom.nextInt(10));
+      btn.setBackgroundColor(0xff000000 | mRandom.nextInt());
+
+      return btn;
+    }
   }
 }
