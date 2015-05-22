9f5b0e05bdec6995aee2b5f7ebc5b6a2a2c5656a
yulu <yulu@utree.cn>
2015-05-22 14:45:02 +0800

    WeekRankResponse

---
+++ data/src/main/java/com/utree/eightysix/data/RankedUser.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2015. All rights reserved by utree.cn
+ */
+
+package com.utree.eightysix.data;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ */
+public class RankedUser extends BaseUser {
+
+  @SerializedName("rank")
+  public int rank;
+
+  @SerializedName("level")
+  public int level;
+
+  @SerializedName("levelIcon")
+  public String levelIcon;
+
+  @SerializedName("experience")
+  public int experience;
+}
