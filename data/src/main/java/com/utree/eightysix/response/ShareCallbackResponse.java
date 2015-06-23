3c8b505377df6b53d55298191d4f08971d76979b
Simon Yu <ccl1115@gmail.com>
2015-06-23 23:02:42 +0800

    ShareCallbackResponse

--- data/src/main/java/com/utree/eightysix/response/ShareCallbackResponse.java
+++ data/src/main/java/com/utree/eightysix/response/ShareCallbackResponse.java
@@ -1,9 +1,18 @@
 package com.utree.eightysix.response;
 
+import com.google.gson.annotations.SerializedName;
 import com.utree.eightysix.rest.Response;
 
 /**
- * Created by simon on 15/6/23.
  */
 public class ShareCallbackResponse extends Response {
+
+  @SerializedName("object")
+  public ShareCallback object;
+
+  public static class ShareCallback {
+
+    @SerializedName("experience")
+    public int experience;
+  }
 }
